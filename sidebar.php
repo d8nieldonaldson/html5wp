6b883bcf61381505a87e220b368c85277dc96e79
Daniel Donaldson <d8nieldonaldson@gmail.com>
2011-03-31 20:24:33 -0400

    sidebar widgetized

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,25 @@
 <div id="sidebar">
 <?php get_search_form(); ?>
+
+<ul id="sidebar">
+<?php if ( !function_exists('dynamic_sidebar')
+        || !dynamic_sidebar() ) : ?>
+ <li id="about">
+  <h2>About</h2>
+  <p>This is my blog.</p>
+ </li>
+ <li id="links">
+  <h2>Links</h2>
+  <ul>
+   <li><a href="http://example.com">Example</a></li>
+  </ul>
+ </li>
+<?php endif; ?>
+</ul>
+
+
+
+
+
+
 </div>
